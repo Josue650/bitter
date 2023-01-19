e832905d1a2e31363f5c82f3b069a7770e66e7e3
Josue <cuellarjosue3@gmail.com>
2023-01-18 18:59:50 -0800

    added PORT

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const path = require('path')
 const favicon = require('server-favicon')
 const logger = require('morgan')
+const PORT = process.env.PORT || 3001
 
 
 const app = express()
