36f6c64022a63ff2b82a8d3fa6b79cb7b38d107b
Josue <cuellarjosue3@gmail.com>
2023-01-18 18:52:53 -0800

    server complete

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+require('./config/database')
+
+const express = require('express')
+const path = require('path')
+const favicon = require('server-favicon')
+const logger = require('morgan')
+
+
+const app = express()
+
+app.use(express.json())
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
+app.use(logger('dev'))
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(express.static(path.join(__dirname, 'build')))
+
+// app.use('/api/users', require('.routes/api/users'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`I am listening on Port: ${PORT}`)
+})
