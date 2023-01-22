169ac13ed614efbf55d1a691c92bc9c6b534244b
Devon Aimes <aimes.devon@gmail.com>
2023-01-22 12:56:34 -0500

    get user tweets added

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const commentCon = require('../../controller/api/comments')
-const ensureLoggedIn = require('../../config/ensureLoggedIn')
-const checkToken = require('../../config/checkToken')
 
-router.get('/:tweetId', checkToken, ensureLoggedIn, commentCon.getAllComments, commentCon.respondWithComments )
-router.delete('/:tweetId/:id', checkToken, ensureLoggedIn, commentCon.destroyComment, commentCon.respondWithComment)
-router.put('/:tweetId/:id', checkToken, ensureLoggedIn, commentCon.updateComment, commentCon.respondWithComment)
-router.post('/:tweetId/', checkToken, ensureLoggedIn, commentCon.createComment, commentCon.respondWithComment)
+router.get('/:tweetId', commentCon.getAllComments, commentCon.respondWithComments )
+router.delete('/:tweetId/:id', commentCon.destroyComment, commentCon.respondWithComment)
+router.put('/:tweetId/:id', commentCon.updateComment, commentCon.respondWithComment)
+router.post('/:tweetId/', commentCon.createComment, commentCon.respondWithComment)
 // router.get('/:id', commentCon.show, commentCon.respondWithComment)
 
 module.exports = router
