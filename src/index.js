1a661f2cfda875c9a47fc5f4828cff96621b3aa4
zcross13 <zalicross24@gmail.com>
2023-01-22 15:31:16 -0600

    zali: added App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css'
-import App from './pages/App/App';
+import App from './App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
