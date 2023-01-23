be9c270e319e096a7205f46b8878affb2061c8e3
zcross13 <zalicross24@gmail.com>
2023-01-22 20:30:41 -0600

    zali: added feature sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import './styles.css'
 
 export default function App(){
     return(
         <div className="appContainer">
+            <Sidebar/>
             <h1>Welcome to Bitter</h1>
         </div>
     )
