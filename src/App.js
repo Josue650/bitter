cc146fc6c1d124b2b50c5b4063385a362dd120f8
zcross13 <zalicross24@gmail.com>
2023-01-24 15:48:06 -0600

    added login/signup component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
 import './styles.css'
-import { useState, useEffect } from "react";
-import Sidebar from "./components/sidebar/Sidebar";
 import Register from "./pages/register/Register";
 
 export default function App() {
 
-    const [user, setUser ] = useState(null)
 
     return (
         <main className="appContainer">
-            {
-                user ?
-                    <>
-                        <Sidebar />
-                        <h1>Welcome to Bitter</h1>
-                    </>
-                    :
-                    <>
-                    <Register setUser={setUser}/>
-                    </>
-            }
+            <Register/>
         </main>
     );
 }
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,95 +1,104 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import Sidebar from "../../components/sidebar/Sidebar"
+import SignUp from "../../components/SignUp/SignUp"
+import Login from "../../components/Login/Login"
 
-export default function Login({setUser}){    
 
-    const handleChangeAuth = (event) => {
-        setCredentials({...credentials, [event.target.name]: event.target.value}) 
-    } 
+export default function Register() {
 
+    const [user, setUser] = useState(null)
     const [credentials, setCredentials] = useState({
-        email: '', 
-        password: '', 
+        email: '',
+        password: '',
     })
     const [token, setToken] = useState('')
+    const [showSignUp, setShowSignUp] = useState(true)
+
+    const handleChangeAuth = (event) => {
+        setCredentials({ ...credentials, [event.target.name]: event.target.value })
+    }
 
     const login = async () => {
-        try{
+        try {
             const response = await fetch('/api/users/login', {
-                method: 'POST', 
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
-                }, 
-                body: JSON.stringify({email: credentials.email, password: credentials.password}) // turn object into data
+                },
+                body: JSON.stringify({ email: credentials.email, password: credentials.password }) // turn object into data
             })
             const tokenResponse = await response.json()
             setToken(tokenResponse)
             localStorage.setItem('token', JSON.stringify(tokenResponse))
-        }catch(error){
+        } catch (error) {
             console.error(error)
         }
     }
 
     const signUp = async () => {
-        try{
+        try {
             const response = await fetch('/api/users', {
-                method: "POST", 
+                method: "POST",
                 headers: {
                     'Content-Type': 'application/json'
-                }, 
-                body: JSON.stringify({...credentials}) 
+                },
+                body: JSON.stringify({ ...credentials })
             })
             const tokenResponse = await response.json()
             setToken(tokenResponse)
             localStorage.setItem('token', JSON.stringify(tokenResponse))
-        }catch(error){
+        } catch (error) {
             console.error(error)
         }
     }
 
+    useEffect(() => {
+        const getToken = () => {
+            const token = window.localStorage.getItem('token')
+            if (!token || token === 'null' || token === 'undefined') return null
+            const payload = JSON.parse(window.atob(token.split('.')[1]))
+            if (payload.exp < Date.now() / 1000) {
+                window.localStorage.removeItem('token')
+                return null
+            }
+            return token
+        }
+        const token = getToken()
+        const data = token ? JSON.parse(window.atob(token.split('.')[1])).user : null
+        setUser(data)
+    }, [])
 
-    return(
+    return (
         <>
-        <h2>Login</h2>
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            login()
-        }}>
-            <input type='text' value={credentials.email} name='email' onChange={handleChangeAuth} placeholder={'Email Here'}></input>
-            <input type='password' value={credentials.password} name='password' onChange={handleChangeAuth}  placeholder={'Password'}></input>
-            <input type='submit' value="Login as Existing User "></input>
-        </form>
-        <h2>SignUp</h2>
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            signUp()
-        }}>
-            <input type='email' value={credentials.email} name='email' onChange={handleChangeAuth}  placeholder={'Email'}></input>
-            <input type='password' value={credentials.password} name='password' onChange={handleChangeAuth}  placeholder={'Password'}></input>
-            <input type='submit' value="Sign Up as New User"></input>
-        </form>
+            {
+                user ?
+                    <Sidebar />
+                    :
+                    <>
+                    <button 
+                        onClick={() => {
+                        setShowSignUp(!showSignUp)
+                    }}>
+                        {showSignUp ? 'Sign Up With A New Account Below or Click Here to Login in as an Existing User' : 'Welcome Back. Login in as an exisiting user or Click Here To Sign Up With A New Account'}
+                    </button>
+                        {
+                            showSignUp
+                                ? <SignUp
+                                    credentials={credentials}
+                                    handleChangeAuth={handleChangeAuth}
+                                    signUp={signUp} 
+                                    setUser={setUser}/>
+                                : <Login
+                                    login={login}
+                                    credentials={credentials}
+                                    handleChangeAuth={handleChangeAuth}
+                                    setUser={setUser}
+                                />
+                        }
+                    </>
+            }
         </>
-        // <div className="loginContainer">
-        //     <h1>Sign in to Bitter</h1>
-        //     <form onSubmit={(e) => {
-        //         e.preventDefault()
-        //         signUp()
-        //     }}>
-        //         <button>Sign in using gmail</button>
-        //         <button>Sign in with Apple</button>
-        //         <h2> or </h2>
-        //         <input type='name' value={credentials.name} name='name' onChange={handleChangeAuth}  placeholder={'Name'}></input>
-        //         <input type='email' value={credentials.email} name='email' onChange={handleChangeAuth}  placeholder={'Email'}></input>
-        //         <input type='password' value={credentials.password} name='password' onChange={handleChangeAuth}  placeholder={'Password'}></input>
-        //         <input type='submit' value="Sign Up as New User"></input>
-        //         {/* <label for='email'>Email</label>
-        //         <input type='email' placeholder="email here" id='email' name='email' />
-        //         <label for='password'>Password</label>
-        //         <input type='password' placeholder="******" id='password' name='password'/> */}
-        //         <button onChange={handleChangeAuth}>Next</button>
-        //     </form>
-
-        //         <h2>Don't have an account?</h2>
-        //         <button className="signupBtn">Sign Here</button>
-        // </div>
     )
 }
+
+
