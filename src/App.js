635ec937582b09158db8a93b78ee53e62c193daf
Mayerlin Duluc <may_duluc@hotmail.com>
2023-01-28 01:36:18 -0500

    Widgets complete

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App(){
     return(
         <div className="appContainer">
             <Sidebar/>
-            <h1>Welcome to Bitter</h1>
+            
 
             <Feed />
 
