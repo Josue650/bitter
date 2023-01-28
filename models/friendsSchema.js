01d6d5bdc877fa96a5297627444ee1a7b2839a9a
Devon Aimes <aimes.devon@gmail.com>
2023-01-28 18:21:46 -0500

    username in user, profile, friends updated

--- models/friendsSchema.js
+++ models/friendsSchema.js
@@ -3,9 +3,10 @@ const friends = require('./friends')
 const Schema = require('mongoose').Schema
 
 const friendsSchema = new Schema({
-  username: { type: String, required: true },
-  photo: { type: String },
-  tweets: [{ type: Schema.Types.ObjectId, ref: 'Tweets' }],
+  // username: { type: String, required: true },
+  // photo: { type: String },
+  // tweets: [{ type: Schema.Types.ObjectId, ref: 'Tweets' }],
+  profile: [{type: Schema.Types.ObjectId, ref:"Profile"}]
 })
 
 module.exports = friendsSchema
