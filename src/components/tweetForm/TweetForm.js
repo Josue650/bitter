b0d6d59bb176d42213a0791e933fe866a6e18839
GitOfAMN <ashleynickens@gmail.com>
2023-01-31 18:52:44 -0500

    delted profile components etc

--- src/components/tweetForm/TweetForm.js
+++ src/components/tweetForm/TweetForm.js
@@ -38,5 +38,5 @@ export default function TweetForm({
                 </div>
             </div>
         </form>
-    );
+    )
 }
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,10 +1,14 @@
-// import Layout from '../components/Layout'
-import ProfileContent from '../components/Profile/ProfileContent'
+import React from 'react'
 
 export default function Profile() {
-  return (
-    <Layout>
-      <ProfileContent />
-    </Layout>
-  )
+    return (
+        <div className='primary'>
+            <h1>Profile</h1>
+            <div className="profile"></div>
+                <div className="profile-info"></div>
+                <div className="profile-head"></div>
+                <div className='profile-img'></div>
+                <div className='followers'></div>
+        </div>
+    )
 }
